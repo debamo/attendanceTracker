a345687b3c3c6fb4cc99d6a1824ec7f0a722652c
Ramusingit <ramachandran5593@gmail.com>
2015-07-08 16:36:52 +0530

    Update AddStudent with fragement

--- app/src/main/java/com/example/owner/attendance_tracker/AddStudent.java
+++ app/src/main/java/com/example/owner/attendance_tracker/AddStudent.java
@@ -1,66 +1,44 @@
-package com.example.owner.attendance_tracker;
+package com.example.owner.project_attendance;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.owner.attendance_tracker.Data.UserHelper;
+import com.example.owner.project_attendance.Data.UserHelper;
 
+public class NextStudent extends Fragment  {
 
-public class AddStudent extends ActionBarActivity {
-    //private StudentDetails stu;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        final EditText name = (EditText) findViewById(R.id.editText);
-        final EditText parents_name = (EditText) findViewById(R.id.editText2);
-        final EditText mobile = (EditText) findViewById(R.id.editText3);
-        final EditText address = (EditText) findViewById(R.id.editText4);
-        Button add = (Button) findViewById(R.id.button);
-        final UserHelper db = new UserHelper(this);
-
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View rootView = inflater.inflate(R.layout.activity_main, container, false);
+        final EditText name = (EditText) rootView.findViewById(R.id.editText);
+        final EditText parents_name = (EditText) rootView.findViewById(R.id.editText2);
+        final EditText mobile = (EditText) rootView.findViewById(R.id.editText3);
+        final EditText address = (EditText) rootView.findViewById(R.id.editText4);
+        Button add = (Button) rootView.findViewById(R.id.button);
+        final UserHelper db = new UserHelper(this.getActivity());
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 db.getWritableDatabase();
                 String stud_name = name.getText().toString();
                 String parent_name =parents_name.getText().toString();
                 String mob = mobile.getText().toString();
                 String addr = address.getText().toString();
                 long result = db.insertStudent(stud_name,parent_name,mob,addr);
-                Toast.makeText(getBaseContext(),"Data Saved",Toast.LENGTH_SHORT).show();
+                db.close();
+                Toast.makeText(getActivity(), "Data Saved", Toast.LENGTH_SHORT).show();
 
             }
         });
 
+        return rootView;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
